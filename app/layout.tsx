3afd6ab8106738436aff1550226401a99bf6048b
Michael Shimeles <michaelwasihun96@gmail.com>
2023-10-01 01:19:23 -0400

    Push

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
+import { ThemeProvider } from "@/components/theme-provider"
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Church Platform',
+  description: 'The Complete Church Platform',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body className={inter.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { NavBar } from '@/components/NavBar'
+
+export default function Home() {
+  return (
+    <main className="flex min-w-screen flex-col items-center justify-between">
+      <NavBar />
+    </main>
+  )
+}
