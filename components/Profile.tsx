d6de0f6cca0e40956d5343d20b51ddac50396699
Michael Shimeles <michaelwasihun96@gmail.com>
2023-10-11 16:55:06 -0400

    Added sample form, mobile optimized navbar

---
+++ components/Profile.tsx
@@ -0,0 +1,70 @@
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+} from "@/components/ui/avatar"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuShortcut,
+    DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu"
+import { SignOutButton, useUser } from "@clerk/nextjs"
+import {
+    CreditCard,
+    LogOut,
+    Settings,
+    User
+} from "lucide-react"
+import Link from "next/link"
+
+export function Profile() {
+    const { isSignedIn, user, isLoaded } = useUser();
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild className="w-[2.25rem] h-[2.25rem]">
+                <Avatar >
+                    <AvatarImage src={user?.imageUrl} alt="User Profile" />
+                    <AvatarFallback></AvatarFallback>
+                </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                    <Link href="/user-profile">
+                        <DropdownMenuItem>
+                            <User className="mr-2 h-4 w-4" />
+                            <span>Profile</span>
+                            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+                        </DropdownMenuItem>
+                    </Link>
+                    <Link href="/donations">
+                        <DropdownMenuItem>
+                            <CreditCard className="mr-2 h-4 w-4" />
+                            <span>Donations</span>
+                            <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+                        </DropdownMenuItem>
+                    </Link>
+                    <DropdownMenuItem>
+                        <Settings className="mr-2 h-4 w-4" />
+                        <span>Settings</span>
+                        <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+                    </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <SignOutButton>
+                    <DropdownMenuItem>
+                        <LogOut className="mr-2 h-4 w-4" />
+                        <span>Log out</span>
+                        <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+                    </DropdownMenuItem>
+                </SignOutButton>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,13 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-  publicRoutes: ["/"],
+  publicRoutes: [
+    "/",
+    "/contact-us",
+    "/about",
+    "/join-us",
+    "/contact-us",
+  ],
 });
 
 export const config = {
