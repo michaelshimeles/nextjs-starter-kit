625293b60709a5e1d36c1066e8fa1a3581e001d5
Michael Shimeles <michaelwasihun96@gmail.com>
2024-01-24 15:14:35 -0500

    Little clean up

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -16,7 +16,7 @@ const SheetClose = SheetPrimitive.Close
 const SheetPortal = ({
   className,
   ...props
-}: SheetPrimitive.DialogPortalProps) => (
+}: any) => (
   <SheetPrimitive.Portal className={cn(className)} {...props} />
 )
 SheetPortal.displayName = SheetPrimitive.Portal.displayName
