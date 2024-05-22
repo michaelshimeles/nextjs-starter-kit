784cdd53666d1cbe6f05e242b42edda2e2120576
Michael Shimeles <michaelwasihun96@gmail.com>
2024-05-22 17:44:30 -0400

    Add seo metadata in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,10 +8,25 @@ import { GeistSans } from 'geist/font/sans';
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Nextjs 14 Starter Template',
-  description: 'Build your next SAAS product',
+  metadataBase: new URL("https://starter.rasmic.xyz"),
+  title: {
+    default: 'Nextjs Starter Kit',
+    template: `%s | Nextjs Starter Kit`
+  },
+  openGraph: {
+    description: 'Build your next SAAS product',
+    images: ['']
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Nextjs Starter ',
+    description: 'Build your next SAAS product.',
+    siteId: "",
+    creator: "@rasmic",
+    creatorId: "",
+    images: [''],
+  },
 }
-
 export default function RootLayout({
   children,
 }: {
