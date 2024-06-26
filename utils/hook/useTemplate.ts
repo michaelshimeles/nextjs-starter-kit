2db3171c5145efafe3f4a3371999e9172f806c9e
Michael Shimeles <michaelwasihun96@gmail.com>
2024-06-26 11:51:09 -0400

    Clean up templates

--- utils/hook/useTemplate.ts
+++ utils/hook/useTemplate.ts
@@ -1,19 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import { actionTemplate } from "../actions/action-template";
 
-async function fetchTemplate() {
-  try {
-    const response = await actionTemplate();
-
-    return response;
-  } catch (error) {
-    return error;
-  }
-}
-
 export const useTemplate = () => {
   return useQuery({
     queryKey: ["get-template"],
-    queryFn: () => fetchTemplate(),
+    queryFn: () => actionTemplate(),
   });
 };
