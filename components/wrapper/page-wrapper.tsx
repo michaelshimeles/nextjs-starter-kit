79c890bd8ece0243015a4f9f4b7d4bf4769635a6
Michael Shimeles <michaelwasihun96@gmail.com>
2024-07-05 16:57:31 -0400

    build 4

--- components/wrapper/page-wrapper.tsx
+++ components/wrapper/page-wrapper.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import NavBar from '../navbar'
 import Footer from '../footer'
+import NavBar from '../navbar'
 
 export default function PageWrapper({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <NavBar />
+     <NavBar />
       <main className="flex min-w-screen flex-col pt-[4rem] items-center dark:bg-black bg-white justify-between">
         <div className="absolute z-[-99] pointer-events-none inset-0 flex items-center justify-center  [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
         {children}
