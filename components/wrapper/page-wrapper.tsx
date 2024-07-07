ee679ff67154c1ccded10034cac995056b206d28
Michael Shimeles <michaelwasihun96@gmail.com>
2024-07-06 21:00:45 -0400

    Moved footer

--- components/wrapper/page-wrapper.tsx
+++ components/wrapper/page-wrapper.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import NavBar from './navbar'
-import Footer from '../footer'
+import Footer from './footer'
 
 export default function PageWrapper({ children }: { children: React.ReactNode }) {
   return (
