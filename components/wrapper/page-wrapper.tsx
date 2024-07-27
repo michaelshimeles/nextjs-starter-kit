b67c743d116304b3786cc226c9c6f4a1ae58953f
Michael Shimeles <michaelwasihun96@gmail.com>
2024-07-27 13:50:21 -0400

    min heigh wrapper

--- components/wrapper/page-wrapper.tsx
+++ components/wrapper/page-wrapper.tsx
@@ -6,7 +6,7 @@ export default function PageWrapper({ children }: { children: React.ReactNode })
   return (
     <>
       <NavBar />
-      <main className="flex min-w-screen flex-col pt-[4rem] items-center dark:bg-black bg-white justify-between">
+      <main className="flex min-w-screen min-h-screen flex-col pt-[4rem] items-center dark:bg-black bg-white justify-between">
         <div className="absolute z-[-99] pointer-events-none inset-0 flex items-center justify-center [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
         {children}
       </main>
