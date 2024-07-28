7d3986bbee6b30f6adc13f4e32cd415e945b50cc
Michael Shimeles <michaelwasihun96@gmail.com>
2024-07-28 11:45:29 -0400

    Updated aria label for button

--- components/homepage/pricing.tsx
+++ components/homepage/pricing.tsx
@@ -90,8 +90,7 @@ const PricingCard = ({ user, handleCheckout, isYearly, title, priceIdMonthly, pr
         onClick={() => handleCheckout(isYearly ? priceIdYearly : priceIdMonthly, true)}
         className="relative inline-flex w-full items-center justify-center rounded-md bg-black text-white dark:bg-white px-6 font-medium dark:text-black transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
         type="button"
-        aria-label={`${actionLabel} - ${isYearly ? 'Yearly' : 'Monthly'} plan`}
-        aria-pressed={isYearly}
+        aria-label={`Select ${isYearly ? 'Yearly' : 'Monthly'} plan`}
       >
         <div className="absolute -inset-0.5 -z-10 rounded-lg bg-gradient-to-b from-[#c7d2fe] to-[#8678f9] opacity-75 blur" />
         {actionLabel}
