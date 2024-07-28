8577ffb0e94332fba36d871277fc724acee53f77
Michael Shimeles <michaelwasihun96@gmail.com>
2024-07-28 01:04:49 -0400

    update mode toggle

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -1,40 +1,36 @@
 "use client"
 
-import * as React from "react"
-import { Moon, Sun } from "lucide-react"
-import { useTheme } from "next-themes"
+import { useEffect, useState } from "react";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
 
-import { Button } from "@/components/ui/button"
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+import { Button } from "@/components/ui/button";
 
-export function ModeToggle() {
-    const { setTheme } = useTheme()
+export default function ModeToggle() {
+    const { theme, setTheme } = useTheme();
+    const [mounted, setMounted] = useState(false);
+
+    // After mounting, we have access to the theme
+    useEffect(() => setMounted(true), []);
+
+    if (!mounted) {
+        // Render nothing on the server and until the theme is mounted
+        return null;
+    }
 
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button variant="outline" size="icon">
-                    <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                    <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <div>
+            {theme === "dark" ? (
+                <Button variant="ghost" className="hover:bg-inherit border-zinc-900 bg-[#0c0c0d]" size="icon" onClick={() => setTheme("light")}>
+                    <Sun className="w-5 h-5" />
                     <span className="sr-only">Toggle theme</span>
                 </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-                <DropdownMenuItem onClick={() => setTheme("light")}>
-                    Light
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme("dark")}>
-                    Dark
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme("system")}>
-                    System
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
-    )
+            ) : (
+                <Button variant="ghost" size="icon" className="hover:bg-inherit border-zinc-100 bg-inherit" onClick={() => setTheme("dark")}>
+                    <Moon className="w-5 h-5" />
+                    <span className="sr-only">Toggle theme</span>
+                </Button>
+            )}
+        </div>
+    );
 }
