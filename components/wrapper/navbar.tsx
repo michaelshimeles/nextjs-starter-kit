cb3f57f53ad8cae4d371a8d52ed89bb39275b424
Michael Shimeles <michaelwasihun96@gmail.com>
2024-07-28 13:35:21 -0400

    updated navbar hamburger

--- components/wrapper/navbar.tsx
+++ components/wrapper/navbar.tsx
@@ -57,7 +57,7 @@ export default function NavBar() {
             <div className="flex justify-between w-full min-[825px]:hidden">
                 <Dialog>
                     <SheetTrigger className="p-2 transition">
-                        <Button size="icon" asChild>
+                        <Button size="icon" variant="ghost" className="w-4 h-4" asChild>
                             <GiHamburgerMenu />
                         </Button>
                     </SheetTrigger>
