178aba6d529731ef3d232866cc8a055d270a0617
Michael Shimeles <michaelwasihun96@gmail.com>
2024-07-29 11:21:13 -0400

    Update document for meta description

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,6 @@ import AuthWrapper from '@/components/wrapper/auth-wrapper'
 import { Analytics } from "@vercel/analytics/react"
 import { GeistSans } from 'geist/font/sans'
 import type { Metadata } from 'next'
-import Head from 'next/head'
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -14,13 +13,15 @@ export const metadata: Metadata = {
     default: 'Nextjs Starter Kit',
     template: `%s | Nextjs Starter Kit`
   },
+  description: 'Everything you need to quickly build your SaaS giving you time to focus on what really matters',
   openGraph: {
     description: 'Everything you need to quickly build your SaaS giving you time to focus on what really matters',
-    images: ['https://utfs.io/f/8a428f85-ae83-4ca7-9237-6f8b65411293-eun6ii.png']
+    images: ['https://utfs.io/f/8a428f85-ae83-4ca7-9237-6f8b65411293-eun6ii.png'],
+    url: 'https://starter.rasmic.xyz/'
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'Nextjs Starter ',
+    title: 'Nextjs Starter',
     description: 'Everything you need to quickly build your SaaS giving you time to focus on what really matters.',
     siteId: "",
     creator: "@rasmic",
@@ -28,6 +29,7 @@ export const metadata: Metadata = {
     images: ['https://utfs.io/f/8a428f85-ae83-4ca7-9237-6f8b65411293-eun6ii.png'],
   },
 }
+
 export default function RootLayout({
   children,
 }: {
@@ -36,7 +38,7 @@ export default function RootLayout({
   return (
     <AuthWrapper>
       <html lang="en" suppressHydrationWarning>
-        <Head>
+        <head>
           <link
             rel="preload"
             href="https://utfs.io/f/31dba2ff-6c3b-4927-99cd-b928eaa54d5f-5w20ij.png"
@@ -47,11 +49,7 @@ export default function RootLayout({
             href="https://utfs.io/f/69a12ab1-4d57-4913-90f9-38c6aca6c373-1txg2.png"
             as="image"
           />
-          <meta property="description" content="Everything you need to quickly build your SaaS giving you time to focus on what really matters" />
-          <meta property="og:image" content="https://utfs.io/f/8a428f85-ae83-4ca7-9237-6f8b65411293-eun6ii.png" />
-          <meta property="og:url" content="https://starter.rasmic.xyz/" />
-          <meta property="twitter:image" content="https://utfs.io/f/8a428f85-ae83-4ca7-9237-6f8b65411293-eun6ii.png" />
-        </Head>
+        </head>
         <body className={GeistSans.className}>
           <Provider>
             <ThemeProvider
