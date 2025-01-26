4af23f605a410cdbe3c47fc65710d1c108ea5cd0
Michael Shimeles <michaelwasihun96@gmail.com>
2025-01-25 22:12:06 -0500

    Change metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,19 +10,19 @@ import './globals.css'
 export const metadata: Metadata = {
   metadataBase: new URL("https://starter.rasmic.xyz"),
   title: {
-    default: 'Nextjs Starter Kit',
-    template: `%s | Nextjs Starter Kit`
+    default: 'Next Starter',
+    template: `%s | Next Starter`
   },
-  description: 'The Ultimate Nextjs 14 Starter Kit for quickly building your SaaS, giving you time to focus on what really matters',
+  description: 'The Ultimate Nextjs 15 Starter Kit for quickly building your SaaS, giving you time to focus on what really matters',
   openGraph: {
-    description: 'The Ultimate Nextjs 14 Starter Kit for quickly building your SaaS, giving you time to focus on what really matters',
+    description: 'The Ultimate Nextjs 15 Starter Kit for quickly building your SaaS, giving you time to focus on what really matters',
     images: ['https://utfs.io/f/8a428f85-ae83-4ca7-9237-6f8b65411293-eun6ii.png'],
     url: 'https://starter.rasmic.xyz/'
   },
   twitter: {
     card: 'summary_large_image',
     title: 'Nextjs Starter Kit',
-    description: 'The Ultimate Nextjs 14 Starter Kit for quickly building your SaaS, giving you time to focus on what really matters',
+    description: 'The Ultimate Nextjs 15 Starter Kit for quickly building your SaaS, giving you time to focus on what really matters',
     siteId: "",
     creator: "@rasmic",
     creatorId: "",
