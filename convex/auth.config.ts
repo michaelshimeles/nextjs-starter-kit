138d0a8d0cbad6848123ecb923c244b1ce2d316a
Michael Shimeles <michaelwasihun96@gmail.com>
2025-02-04 13:10:26 -0500

    Updated auth.config

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,8 +1,10 @@
 export default {
-    providers: [
-      {
-        domain: "https://trusty-rabbit-71.clerk.accounts.dev",
-        applicationID: "convex",
-      },
-    ]
-  };
+  providers: [
+    {
+      domain: process.env.NODE_ENV === 'production' ? process.env.CLERK_SIGNING_KEY : "https://trusty-rabbit-71.clerk.accounts.dev",
+    },
+    {
+      applicationID: "convex",
+    },
+  ]
+};
