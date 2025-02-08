78efa73adadc9fafae82a8a0a3035dac065b8a62
Michael Shimeles <michaelwasihun96@gmail.com>
2025-02-08 12:18:20 -0500

    Optimize

--- app/(pages)/success/page.tsx
+++ app/(pages)/success/page.tsx
@@ -1,29 +1,29 @@
-'use client';
-
 import { Button } from '@/components/ui/button';
 import NavBar from '@/components/wrapper/navbar';
-import { useAuth } from '@clerk/nextjs';
-import Link from 'next/link';
 import { api } from '@/convex/_generated/api';
-import { useQuery } from 'convex/react';
+import { getAuthToken } from '@/lib/auth';
+import { fetchQuery } from 'convex/nextjs';
+import Link from 'next/link';
 
-export default function SuccessPage() {
-  const { userId } = useAuth();
-  const subscriptionStatus = useQuery(api.subscriptions.getUserSubscriptionStatus);
+export default async function SuccessPage() {
+  const token = await getAuthToken();
 
-  const canAccessDashboard = userId && subscriptionStatus?.hasActiveSubscription;
+  const { hasActiveSubscription } = await fetchQuery(api.subscriptions.getUserSubscriptionStatus, {
+  }, {
+    token: token!,
+  });
+
 
   return (
     <main className="flex min-w-screen flex-col items-center justify-between">
       <NavBar />
-      <h1 className="mt-[35vh] mb-3 scroll-m-20  text-5xl font-semibold tracking-tight transition-colors first:mt-0">
-        Welcome to Nextjs Starter Kit 🎉
-      </h1>
-      <p className="leading-7 text-center w-[60%]">
-        Let&apos;s get cooking
-      </p>
-      <Link href={canAccessDashboard ? "/dashboard" : "/pricing"} className='mt-4'>
-        <Button>{canAccessDashboard ? "Access Dashboard" : "View Pricing"}</Button>
+      {hasActiveSubscription ? <h1 className="mt-[35vh] mb-3 scroll-m-20  text-5xl font-semibold tracking-tight transition-colors first:mt-0">
+        Subscription Successful 🎉
+      </h1> : <h1 className="mt-[35vh] mb-3 scroll-m-20  text-5xl font-semibold tracking-tight transition-colors first:mt-0">
+        You Can Subscribe Now
+      </h1>}
+      <Link href={hasActiveSubscription ? "/dashboard" : "/pricing"} className='mt-4'>
+        <Button>{hasActiveSubscription ? "Access Dashboard" : "View Pricing"}</Button>
       </Link>
     </main>
   )
